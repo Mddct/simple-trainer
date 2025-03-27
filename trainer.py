7ed048805bf385246678dd1b57f36e9172299d2f
Dinghao Zhou <hamddct@gmail.com>
2025-03-27 14:02:34 +0000

    add eval

--- trainer.py
+++ trainer.py
@@ -15,6 +15,7 @@ from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
 from utils import if_print_model, init_distributed, wrap_cuda_model
 from writer import create_default_writer
+import torch.distributed as dist
 
 
 class TrainState(Stateful):
@@ -26,8 +27,8 @@ class TrainState(Stateful):
     and optimizer.
     """
 
-    def __init__(self, model, optimizer=None):
-        self.model = model
+    def __init__(self, model: torch.nn.Module, config, optimizer=None):
+        self.model = wrap_cuda_model(model, config)
         self.optimizer = optimizer
         self._step = 0
 
@@ -48,12 +49,20 @@ class TrainState(Stateful):
     def train_step(self, batch: dict):
         pass
 
+    @classmethod
+    def eval_step(self, batch: dict):
+        pass
+
     @property
     def step(self):
         return self._step
 
     @classmethod
     def reduce_train_metrics(self, metrics: dict):
+        pass
+
+    @classmethod
+    def reduce_eval_metrics(self, metrics: dict):
         pass
 
     @property
@@ -100,21 +109,15 @@ class Trainer:
                  dataloader_or_dataset,
                  eval_datalader_or_dataset=None):
         self.config = config
-
         self.world_size, self.rank, self.local_rank = init_distributed(config)
-        if_print_model(
-            config,
-            state.model,
-        )
-        self.model = wrap_cuda_model(self.train_state.model, config)
-
         self.writer = create_default_writer(config.tensorboard_dir,
                                             just_logging=self.rank != 0,
                                             asynchronous=True)
         self.train_iter = iter(dataloader_or_dataset)
-
+        self.eval_iter = None
+        if eval_datalader_or_dataset is not None:
+            self.eval_iter = iter(eval_datalader_or_dataset)
         self.train_state = state
-
         self.checkpoint_manager = CheckpointManager(
             config.model_dir,
             "model",
@@ -131,54 +134,51 @@ class Trainer:
             return
         train_metrics_last_t = time.time()
 
-        if isinstance(self.train_state._model,
-                      torch.nn.parallel.DistributedDataParallel):
-            model_context = self.model.join
-        else:
-            model_context = nullcontext
-
-        train_metrics_last_t = time.time()
         train_metrics = []
-        with model_context():
-            for step, batch in zip(range(steps_offset, self.config.max_steps),
-                                   self.train_iter):
 
-                # Disable gradient synchronizations across DDP processes.
-                # Within this context, gradients will be accumulated on module
-                # variables, which will later be synchronized.
-                if (step + 1) % self.config.accum_grad != 0:
-                    context = self.model.no_sync
-                # Used for single gpu training and DDP gradient synchronization
-                # processes.
-                else:
-                    context = nullcontext
+        self.train_state.model.train()
+        for step, batch in zip(range(steps_offset, self.config.max_steps),
+                               self.train_iter):
+            if (step + 1) % self.config.accum_grad == 0:
+                self.train_state.model.set_requires_gradient_sync(True)
+            else:
+                self.train_state.model.set_requires_gradient_sync(False)
+            metrics = self.train_state.train_step(batch)
+            train_metrics.append(metrics)
+            if (step + 1) % self.config.log_interval == 0:
+                m = self.train_state.reduce_train_metrics(train_metrics)
+                summary = {f"train/{k}": v for k, v in m.items()}
+                summary['steps_per_second'] = self.config.log_interval / (
+                    time.time() - train_metrics_last_t)
+                self.writer.write_scalars(step + 1, summary)
+                train_metrics = []
+                train_metrics_last_t = time.time()
 
-                with context():
-                    metrics = self.train_state.train_step(batch)
-                train_metrics.append(metrics)
-                if (step + 1) % self.config.log_interval == 0:
-                    m = self.train_state.reduce_train_metrics(train_metrics)
-                    summary = {k: v for k, v in m.items()}
-                    summary['steps_per_second'] = self.config.log_interval / (
-                        time.time() - train_metrics_last_t)
-                    self.writer.write_scalars(step + 1, summary)
-                    train_metrics = []
-                    train_metrics_last_t = time.time()
-
-                if (
-                        step + steps_offset + 1
+            if (step + steps_offset + 1
                 ) % self.config.save_interval == 0 and steps_offset != 0 and (
-                        step + 1) % self.config.accum_grad == 0:
-                    import torch.distributed as dist
-                    self.checkpoint_manager.save(self.train_state)
-                    dist.barrier()
+                    step + 1) % self.config.accum_grad == 0:
 
-                if (
-                        step + steps_offset + 1
+                self.checkpoint_manager.save(self.train_state)
+                dist.barrier()
+
+            if (step + steps_offset + 1
                 ) % self.config.eval_interval == 0 and steps_offset != 0 and (
-                        step + 1) % self.config.accum_grad == 0:
-                    # TODO: run evaluation
-                    # import torch.distributed as dist
-                    # self.train_state.save()
-                    # dist.barrier()
-                    pass
+                    step + 1
+                ) % self.config.accum_grad == 0 and self.eval_iter is not None:
+                eval_metrics_last = time.time()
+                self.train_state.model.eval()
+                with torch.no_grad():
+                    for (i, eval_batch) in enumerate(self.eval):
+                        eval_metrics = self.train_state.eval_step(batch)
+                        if (i + 1) % self.config.log_interval == 0:
+                            m = self.train_state.reduce_train_metrics(
+                                eval_metrics)
+                            summary = {f"eval/{k}": v for k, v in m.items()}
+                            summary[
+                                'steps_per_second'] = self.config.log_interval / (
+                                    time.time() - eval_metrics_last)
+                            self.writer.write_scalars(step + 1, summary)
+                            eval_metrics = []
+                            eval_metrics_last = time.time()
+                    dist.barrier()
+                self.train_state.model.train()
